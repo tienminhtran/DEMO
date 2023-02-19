e13dc8ea2b0029c19752c0cb16ae1fa91d09b460
tienminhtran <tientot36@gmail.com>
2023-02-19 21:58:07 +0700

    add content for tt

--- CapSoMayMan.cpp
+++ CapSoMayMan.cpp
@@ -11,6 +11,7 @@ int main()
         fscanf(f, "%d%d", &n1, &n2);
         if ((n1 + n2) % 3 == 0)
         {
+            printf("TTTT");
             printf("(%d, %d)", n1, n2);
         }
     } while (f == NULL);
